b41961cd746e42d09dd240c8e044a90809d3999b
Ruslana <ruslana.chumachenko@gmail.com>
2020-09-15 00:41:12 +0300

    Initial commit

---
+++ src/main/java/JiraJSONObjects.java
@@ -0,0 +1,25 @@
+import org.json.simple.JSONObject;
+
+public class JiraJSONObjects {
+
+  public static String newIssueJSON() {
+    JSONObject newIssueJSON = new JSONObject();
+    JSONObject fields = new JSONObject();
+
+    fields.put("summary", "API test summary");
+    JSONObject issueType = new JSONObject();
+    issueType.put("id", "10105");
+    issueType.put("name", "test");
+    JSONObject project = new JSONObject();
+    project.put("id", "10508");
+    JSONObject reporter = new JSONObject();
+    reporter.put("name", "RuslanaChumachenko");
+
+    fields.put("issuetype", issueType);
+    fields.put("project", project);
+    fields.put("reporter", reporter);
+
+    newIssueJSON.put("fields", fields);
+    return newIssueJSON.toJSONString();
+  }
+}
---
+++ src/test/java/JiraAPITests.java
@@ -0,0 +1,67 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class JiraAPITests {
+  String ticketId;
+  String newIssue = JiraJSONObjects.newIssueJSON();
+
+  @Test
+  public void createIssue() {
+    Response response =
+        given().
+            auth().preemptive().basic("RuslanaChumachenko", "RuslanaChumachenko").
+            contentType(ContentType.JSON).
+            body(newIssue).
+            when().
+            post("https://jira.hillel.it/rest/api/2/issue").
+            then().
+            contentType(ContentType.JSON).
+            statusCode(201).
+            extract().response();
+    response.prettyPrint();
+    ticketId = response.path("id");
+    System.out.println("Ticket ID: " + ticketId);
+
+    Response response2 =
+        given().
+            auth().preemptive().basic("RuslanaChumachenko", "RuslanaChumachenko").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            contentType(ContentType.JSON).
+            statusCode(200).
+            extract().response();
+    response2.prettyPrint();
+    assertEquals(response2.path("fields.summary"), "API test summary");
+    assertEquals(response2.path("fields.creator.name"), "RuslanaChumachenko");
+
+
+    Response deleteIssueResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            contentType(ContentType.JSON).
+            when().
+            delete("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            statusCode(204).
+            extract().response();
+    deleteIssueResponse.print();
+
+    //Get deleted issue
+    Response checkIfIssueDeletedResponse =
+        given().
+            auth().preemptive().basic("webinar5", "webinar5").
+            contentType(ContentType.JSON).
+            when().
+            get("https://jira.hillel.it/rest/api/2/issue/" + ticketId).
+            then().
+            statusCode(404).
+            extract().response();
+  }
+}
+
