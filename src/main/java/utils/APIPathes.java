a400e2291056bdb5e26351bc8309e9b1b1ff1ad4
Ruslana <ruslana.chumachenko@gmail.com>
2020-09-28 23:08:41 +0300

    added TestNGListener

--- src/main/java/utils/APIPathes.java
+++ src/main/java/utils/APIPathes.java
@@ -2,7 +2,7 @@ package utils;
 
 public interface APIPathes {
   //String baseURL = "https://jira.hillel.it";
-  String baseURL = null;
+  String baseURL = YamlReader.getEnvironment(System.getenv("environment"));
   String issue = baseURL + "/rest/api/2/issue/";
   String comment = issue + "%s/comment/";
 }
